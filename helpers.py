7f5ef974e91216cbf18842552fd90aeb65f710cb
shaoxiongji <shaoxiong.ji@foxmail.com>
2018-02-01 10:16:23 +1100

    add helpers and options

---
+++ helpers.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+import csv
+import numpy as np
+import pandas as pd
+import datetime
+from sklearn import metrics
+
+
+def load_df(dataset_name):
+    """
+    Load data as DataFrame for logistic regression and ensemble models, without sampling
+    :param dataset_name: name of dataset
+    :return: DataFrame
+    """
+    print('Loading dataset DataFrame')
+    if dataset_name == 'EP' or dataset_name == 'ep':
+        file_name = './data/experience_project/ep.csv'
+        df = pd.read_csv(file_name)
+        return df
+    elif dataset_name == 'twitter' or dataset_name == 'Twitter':
+        file_name = './data/twitter/twitter.xlsx'
+        df = pd.read_excel(file_name)
+        return df
+    elif dataset_name in ['popular', 'AskReddit', 'books', 'gaming', 'movies']:
+        file_name = './data/reddit/{}.csv'.format(dataset_name)
+        df = pd.read_csv(file_name)
+        return df
+    else:
+        raise ValueError("Error: unrecognized dataset")
+
+
+def load_data(dataset_name):
+    """
+    Load data for NN models, with under sampling from Twitter dataset
+    :param dataset_name: name of dataset
+    :return: X, y
+    """
+    print('Loading text dataset')
+    if dataset_name == 'EP' or dataset_name == 'ep':
+        file_name = './data/experience_project/ep.csv'
+        df = pd.read_csv(file_name)
+        X = df['body'].as_matrix()
+        y = df['y'].as_matrix()
+        return X, y
+    elif dataset_name == 'twitter' or dataset_name == 'Twitter':
+        file_name = './data/twitter/twitter.xlsx'
+        df = pd.read_excel(file_name)
+        df = df.dropna()
+        df_pos = df.loc[df['y'] == 1]
+        df_neg = df.loc[df['y'] == 0]
+        df = pd.concat([df_pos, df_neg.sample(len(df_pos['y']))])
+        df = df.sample(frac=1)
+        X = df['tweets'].as_matrix()
+        y = df['y'].as_matrix()
+        return X, y
+    else:
+        raise ValueError("Error: unrecognized dataset")
+
+
+def find_threshold(fpr, tpr, threshold):
+    rate = np.array(tpr) + np.array(fpr)
+    return threshold[np.argmax(rate)]
+
+
+def evaluate_prediction(y_test, y_pred, k_th, model_name, dataset_name):
+    fpr, tpr, th = metrics.roc_curve(y_test, y_pred)
+    roc_auc = metrics.auc(fpr, tpr)
+
+    df_results = pd.DataFrame({'y_test': y_test, 'y_pred': y_pred}, index=None)
+    df_results.to_csv('./save/prediction_{}_{}_{}.csv'.format(dataset_name, model_name, k_th), index=False)
+
+    o_threshold = 0.5
+    for i in range(len(y_pred)):
+        if y_pred[i] >= o_threshold:
+            y_pred[i] = 1
+        else:
+            y_pred[i] = 0
+
+    acc = metrics.accuracy_score(y_test, y_pred)
+    pre = metrics.precision_score(y_test, y_pred)
+    rec = metrics.recall_score(y_test, y_pred)
+    f1 = metrics.f1_score(y_test, y_pred)
+
+    dict_eval = {'date': datetime.date.today(),
+                 'model': model_name,
+                 'accuracy': acc,
+                 'precision': pre,
+                 'recall': rec,
+                 'f-score': f1,
+                 'roc': roc_auc,
+                 'note': '{}_th fold'.format(k_th),
+                 'dataset': dataset_name
+                 }
+    with open('./output/{}.csv'.format(dataset_name), 'a') as f:
+        field_names = ['date', 'model', 'accuracy', 'precision', 'recall', 'f-score', 'roc', 'note', 'dataset']
+        writer = csv.DictWriter(f, fieldnames=field_names)
+        writer.writerow(dict_eval)
+    return acc, pre, rec, f1, roc_auc
---
+++ options.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+import argparse
+
+
+def arg_clf():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='reddit', help="name of dataset")
+    parser.add_argument('--num_features', type=int, default=1, help="number of features groups")
+    parser.add_argument('--num_topics', type=int, default=10, help="number of topics")
+    args = parser.parse_args()
+    return args
+
+
+def arg_cnn():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_seq_len', type=int, default=300, help="max length of sequences")
+    parser.add_argument('--max_num_words', type=int, default=20000, help="max number of words")
+    parser.add_argument('-d', '--embedding_dim', type=int, default=100, help="embedding dimension")
+    parser.add_argument('--num_filters', type=int, default=128, help="the number output of filters in the convolution")
+    parser.add_argument('--dropout_rate', type=float, default=0.1, help='dropout rate')
+    parser.add_argument('--dense_units', type=int, default=32, help="units of Dense layer")
+
+    parser.add_argument('--dataset', type=str, default='reddit', help="name of dataset")
+    embedding_w2v = '/data/shji/datasets/word2vec/GoogleNews-vectors-negative300.bin'
+    parser.add_argument('-f', '--embedding_file', type=str, default=embedding_w2v, help="embedding file")
+    parser.add_argument('--embedding_type', type=str, default='word2vec', help="the type of word embedding")
+    parser.add_argument('--valid_split', type=float, default=0.1, help="ratio of validation split")
+    parser.add_argument('--kernel_size', type=float, default=3, help='size of kernel')
+    parser.add_argument('--batch_size', type=int, default=64, help="batch size")
+    parser.add_argument('--epochs', type=int ,default=200, help="training epochs")
+    args = parser.parse_args()
+    return args
+
+
+def arg_rnn():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_seq_len', type=int, default=1000, help="max length of sequences")
+    parser.add_argument('--max_num_words', type=int, default=20000, help="max number of words")
+    parser.add_argument('-d', '--embedding_dim', type=int, default=300, help="embedding dimension")
+    parser.add_argument('--rnn_units', type=int, default=128, help="units of RNN")
+    parser.add_argument('--dropout_rate', type=float, default=0.1, help='dropout rate')
+    parser.add_argument('--dense_units', type=int, default=32, help="units of Dense layer")
+
+    parser.add_argument('--dataset', type=str, default='twitter', help="name of dataset")
+    embedding_file = '/data/shji/datasets/word2vec/GoogleNews-vectors-negative300.bin'
+    parser.add_argument('-f', '--embedding_file', type=str, default=embedding_file, help="embedding file")
+    parser.add_argument('--embedding_type', type=str, default='word2vec', help="the type of word embedding")
+    parser.add_argument('--valid_split', type=float, default=0.1, help="ratio of validation split")
+    parser.add_argument('-act', '--activation', type=str, default='relu', help="type of activation function")
+    parser.add_argument('--patience', type=int, default=10, help="number of epochs with no improvement after which training will be stopped")
+    parser.add_argument('--batch_size', type=int, default=64, help="batch size")
+    parser.add_argument('--epochs', type=int ,default=200, help="training epochs")
+    args = parser.parse_args()
+    return args
+
+
+def arg_lstm():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_seq_len', type=int, default=1000, help="max length of sequences")
+    parser.add_argument('--max_num_words', type=int, default=20000, help="max number of words")
+    parser.add_argument('-d', '--embedding_dim', type=int, default=300, help="embedding dimension")
+    parser.add_argument('--lstm_units', type=int, default=128, help="units of LSTM")
+    parser.add_argument('--dropout_rate', type=float, default=0.1, help='dropout rate')
+    parser.add_argument('--dense_units', type=int, default=32, help="units of Dense layer")
+
+    parser.add_argument('--dataset', type=str, default='twitter', help="name of dataset")
+    embedding_file = '/data/shji/datasets/word2vec/GoogleNews-vectors-negative300.bin'
+    parser.add_argument('-f', '--embedding_file', type=str, default=embedding_file, help="embedding file")
+    parser.add_argument('--embedding_type', type=str, default='word2vec', help="the type of word embedding")
+    parser.add_argument('--valid_split', type=float, default=0.1, help="ratio of validation split")
+    parser.add_argument('-act', '--activation', type=str, default='relu', help="type of activation function")
+    parser.add_argument('--patience', type=int, default=10, help="number of epochs with no improvement after which training will be stopped")
+    parser.add_argument('--batch_size', type=int, default=64, help="batch size")
+    parser.add_argument('--epochs', type=int ,default=200, help="training epochs")
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    a = arg_lstm()
